9365fdccfcf19a963ef39109ad37525c43ac1587
boijuny <sixjunee6@gmail.com>
2024-06-23 23:54:23 +0200

    custom

---
+++ data/index.ts
@@ -0,0 +1,259 @@
+export const navItems = [
+  { name: "About", link: "#approach" },
+  { name: "Work", link: "#work" },
+  { name: "Experience", link: "#experience" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    description: "Pose estimation result on SPEC2021 dataset",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/sat.png",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Spacecraft Detection and Pose Estimation",
+    description: "R&D Project at SII Group, Toulouse, France",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Vision & AI Tech Stack",
+    description: "This project was developed using",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "/grid.svg",
+    spareImg: "",
+    leftLists :["Pytorch", "YOLOv8", "Blender"],
+    rightLists : ["AWS", "OpenCV", "Docker"]
+    
+  },
+  {
+    id: 4,
+    title: "Visual Servo-Control of Manipulator Robot on Tree-like Structure ",
+    description: "Research Project at LAAS-CNRS, Toulouse, France",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    description: "Gazebo simulation of the visual servoing task",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end md:justify-end lg:justify-end",
+    img: "/sim.png",
+    spareImg: "",
+  },
+    {
+    id: 6,
+    title: " Robotics Tech Stack",
+    description: "This project was developed using",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "/grid.svg",
+    spareImg: "",
+    leftLists :  ["PBVS", "ROS", "GraphMatching"],
+    rightLists : ["Gazebo", "C++", "OpenCV"],
+  },
+   {
+    id: 7,
+    title: "",
+    description: "Stratospheric balloon launch.",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/bal.png",
+    spareImg: "",
+  },
+  {
+    id: 8,
+    title: "Onboard System Architecture",
+    description: "Student Project with CNES, Toulouse, France",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "/b4.svg",
+  },
+  {
+    id: 9,
+    title: "Critical Systems Tech Stack",
+    description: "This project was developed using",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "/grid.svg",
+    spareImg: "",
+    leftLists : ["Safety", "Microcontrollers", "Software"],
+    rightLists : ["Critical", "Modems", "Architecture"],
+  },
+
+  {
+    id: 10,
+    title: "Let's make your next project our shared success!",
+    description: "",
+    className: "lg:col-span-5 lg:row-span-0.5 md:col-span-6 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "3D Solar System Planets to Explore",
+    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Yoom - Video Conferencing App",
+    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "AI Image SaaS - Canva Application",
+    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest Tech Stack.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Computer Vision Engineer - Internship",
+    desc: "Research on state-of-the-art strategies for satellite guidance by an onboard monocular camera. - 2024",
+    className: "md:col-span-2",
+    thumbnail: "/sii.svg",
+  },
+  {
+    id: 2,
+    title: "Short-tem Research Project",
+    desc: "Research work on visual servo-control of a manipulator robot on tree-like structures. - 2024",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/laas.svg",
+  },
+  {
+    id: 3,
+    title: "Concept Sotre Manager - Gap Year Internship ",
+    desc: "Assistant in charge of the physical and digital space of the POLKA photography gallery - 2023, Gap year",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/polka.svg",
+  },
+  {
+    id: 4,
+    title: "Customer Success Engineer - Internship",
+    desc: "Assistant engineer for Verteego's decision intelligence SaaS platform (DIP). - 2022, Gap year",
+    className: "md:col-span-2",
+    thumbnail: "/verteego.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/link.svg",
+  },
+];
